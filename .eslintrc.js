1886825f24c60c2afbcb1886231a265edf99d2b5
RedheadBeardJr <johannesbaltussen@gmail.com>
2025-12-30 15:52:29 +0000

    chore: add ESLint, Prettier, husky and basic tests

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off'
+  }
+};
